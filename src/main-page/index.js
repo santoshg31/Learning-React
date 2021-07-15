28eb2eebb39a499b78da89c530a8721ecbb3d5c9
Santosh Gawde <santosh.gawde@sage.com>
2021-07-15 08:47:22 +0100

    Code structuring and header component

---
+++ src/main-page/index.js
@@ -0,0 +1,12 @@
+import './main-page.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
