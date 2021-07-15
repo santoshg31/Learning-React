c9a889c68048971e3cdbe56b100e6bdafdd19074
Santosh Gawde <santosh.gawde@sage.com>
2021-07-15 12:03:17 +0100

    using react hooks

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,7 +1,26 @@
 import './main-page.css';
 import Header from './header';
+import { useEffect, useState, useMemo } from "react";
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);  //useState is used to access the value of houses below , also note the new type of destructuring where allHouses is the value that setAllHouses function will return.
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);  // blank array ("[]") ensures that the houses are loaded only once even if the component is re-rendered again if props values changes
+
+  const featuredHouse = useMemo(() => {   //useMemo is kind of caching used here so that when the component is re - rendered it will show the same featured house from the cache on the main-page.
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
   return (
     <div className="container">
       <Header 
