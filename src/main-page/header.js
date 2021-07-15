88805dc57662db1a2a216e6cc77a18d6242d11fa
Santosh Gawde <santosh.gawde@sage.com>
2021-07-15 09:02:31 +0100

    using class component

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,14 +1,28 @@
 import logo from "./GloboLogo.png";
+import { Component } from "react";
 
-const Header = ({subtitle,title}) => (    //here we used destructing instead of writing props object
-  <header className="row">
-    <div className="col-md-5">
-      <img src={logo} className="logo" alt="logo" />
-    </div>
-    <div className="col-md-7 mt-5 subtitle">
-      {subtitle}{title}
-    </div>
-  </header>
-);
+// const Header = ({subtitle,title}) => (    //here we used destructing instead of writing props object
+//   <header className="row">
+//     <div className="col-md-5">
+//       <img src={logo} className="logo" alt="logo" />
+//     </div>
+//     <div className="col-md-7 mt-5 subtitle">
+//       {subtitle}{title}
+//     </div>
+//   </header>
+// );
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="row">
+        <div className="col-md-5">
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className="col-md-7 mt-5 subtitle">{this.props.subtitle}</div>
+      </header>
+    );
+  }
+}
 
 export default Header;
