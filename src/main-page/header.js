6a016f43eb955d77c1c55dd003bbcb859cf3030c
Santosh Gawde <santosh.gawde@sage.com>
2021-07-15 08:58:08 +0100

    destructuring props

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,12 +1,12 @@
 import logo from "./GloboLogo.png";
 
-const Header = () => (
+const Header = ({subtitle,title}) => (    //here we used destructing instead of writing props object
   <header className="row">
     <div className="col-md-5">
       <img src={logo} className="logo" alt="logo" />
     </div>
     <div className="col-md-7 mt-5 subtitle">
-      Providing houses all over the world
+      {subtitle}{title}
     </div>
   </header>
 );
